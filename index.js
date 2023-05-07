c3e5915d0b792ae012d5ac1b7e711fd5a5a19ab8
lkrum <leahkrumholz73@gmail.com>
2023-05-07 12:14:01 -0500

    wrote alias name for employee table

--- index.js
+++ index.js
@@ -67,19 +67,32 @@ function viewDepartments() {
 
 // function for viewing all roles
 function viewRoles() {
-  db.query(`SELECT role.id, role.title, role.salary, department.name AS department FROM role INNER JOIN department ON role.department_id = department.id`, function (err, results) {
+  db.query(`
+  SELECT role.id, 
+  role.title, 
+  role.salary, 
+  department.name AS department 
+  FROM role 
+  INNER JOIN department ON role.department_id = department.id`, function (err, results) {
     console.table(results);
   });
 }
 
 // function for viewing all employees
+// referneced stackoverflow for how to create an alias name
 function viewEmployees() {
   db.query(`
-SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ' , manager.last_name) AS manager
+SELECT employee.id, 
+employee.first_name, 
+employee.last_name, 
+role.title, 
+department.name AS department, 
+role.salary, 
+CONCAT(manager.first_name, ' ' , manager.last_name) AS manager
 FROM employee
 LEFT JOIN role ON employee.role_id = role.id
 LEFT JOIN department ON role.department_id = department.id
-LEFT JOIN employee manager ON manager.id = employee.manager_id;`, function (err, results) {
+LEFT JOIN employee AS manager ON manager.id = employee.manager_id;`, function (err, results) {
     console.table(results);
   });
 }
